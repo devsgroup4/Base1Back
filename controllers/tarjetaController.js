4988efd1b62095c7ceeb41108148cfa83790d3db
Gimena <mgulb@live.com.ar>
2020-12-27 12:50:05 -0300

    primer commit

---
+++ controllers/tarjetaController.js
@@ -0,0 +1,40 @@
+// SDK de Mercado Pago
+const mercadopago = require ('mercadopago');
+
+
+exports.mercadodepago=(req,res)=>{
+ //number
+ const {entero}=req.query;
+//console.log(req.body)
+ console.log(parseInt(entero))
+const  entero1=parseInt(entero);
+ 
+let preference = {
+  items: [
+    {
+      title: 'Varios',
+      unit_price:entero1,
+      quantity: 1
+    }
+  ]
+}
+
+preference.back_urls={"success":"http://localhost:3000/home"}
+preference.auto_return="approved";
+
+mercadopago.preferences.create(preference)
+
+.then(function(response){
+// Este valor reemplazará el string "<%= global.id %>" en tu HTML
+  global.id = response.body.id;
+  console.log("glonal=",global.id)
+  console.log(response.body.init_point)
+  const dire=response.body.init_point;
+  res.json({ msg: "Compra Completada ", dire});
+  
+  res.send(response.body.init_point);
+}).catch(function(error){
+  console.log(error);
+});
+
+}
